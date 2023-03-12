8fb9dfeb630847ff94c3d10faf74a25cb3d0f79c
pullpanther <softechstar.wlc@outlook.com>
2023-03-13 00:13:22 +0530

    displaying links

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+
 
 # Create your views here.
+
+def scrape(request):
+    page = requests.get("https://www.google.com/")
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    link_address = []
+
+    print("soup:", soup)
+
+    for link in soup.find_all('a'):
+        link_address.append(link.get('href'))
+
+    return render(request, 'myapp/result.html', {'link_address': link_address})
